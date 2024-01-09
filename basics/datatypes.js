3d60225d34f87b5319d7d84e7561d308b4e5ce57
Mannat Chopra <85258935+mannat-chopra@users.noreply.github.com>
2024-01-09 18:55:24 +0000

    datatype Notes

---
+++ basics/datatypes.js
@@ -0,0 +1,12 @@
+//number => 2 to the power 53
+//bigint
+//string
+//boolean => true/false
+//null => Standalone value
+//undefined => 
+//symbol => unique
+//object =>
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object 
