9016e2320e2e38b09fe2dc6ec616e66d8eec8d8a
Mannat Chopra <85258935+mannat-chopra@users.noreply.github.com>
2024-01-09 20:04:19 +0000

    operation in javascript learn

---
+++ basics/conversionOperation.js
@@ -0,0 +1,39 @@
+let score = undefined;
+
+
+//console.log(typeof score);
+// Also a way to write typeof an datatype.
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"23" => 23
+//"23abc" => NaN
+//true => 1; false => 0;
+
+let isLoggedIn = "mannat";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false;
+// "" => false;
+//"mannat => true;
+
+
+// ****************************conversions*******************
+// Arithmetic Operation
+
+// 1 + "2" => 12
+
+// 3 + 3 - 3 * 3 / 3 +( 2 + 2 )
+
+// +true  => 1
+
+// +""  => 0
+
+// Prefix increment
+
+// Postfix increment 
+
