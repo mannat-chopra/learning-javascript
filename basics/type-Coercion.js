e20c6bc9459cf992f131a8fb52bd29c9f54375a7
Mannat Chopra <85258935+mannat-chopra@users.noreply.github.com>
2024-01-12 11:34:40 +0000

    type-Coercion learn

---
+++ basics/type-Coercion.js
@@ -0,0 +1,24 @@
+//Type Coercion
+
+console.log(5+5);
+//10
+
+console.log('5' + 5);
+// string + number = string;
+
+console.log('mannat' + 'chopra');
+// string + string = string (concatination)
+// mannatchopra
+
+console.log('55' - 10);
+// string - number = number; opposite of addition
+// '55' -10 = 45;
+
+
+console.log('4' * 2);
+// string * number = number;
+// '4' * 2 = 8 
+
+console.log('4' / 2);
+// string / numnber = number;
+//'4' / 2 = 2;
